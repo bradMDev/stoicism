118e6c8fb2ec7af0a54ffaceaf7ee6342b216154
Brad Manning <bradmdev@gmail.com>
2024-02-23 19:51:38 -0600

    update quotes

--- stoicisms.go
+++ stoicisms.go
@@ -1,9 +1,9 @@
 package stoicisms
 
 func SayAurelius() string {
-	return ""
+	return "The best revenge is to be unlike him who performed the injury."
 }
 
 func SaySeneca() string {
-	return ""
+	return "A wise man is content with his lot, whatever it may be, without wishing for what he has not."
 }
